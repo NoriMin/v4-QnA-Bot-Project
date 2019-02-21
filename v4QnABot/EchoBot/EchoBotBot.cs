1d94f053535b9f39d531849ef574a254be90fc9e
Takunori <t-tamina@microsoft.com>
2019-02-21 14:59:11 +0900

    プロジェクト ファイルを追加します。

---
+++ v4QnABot/EchoBot/EchoBotBot.cs
@@ -0,0 +1,68 @@
+﻿// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using EchoBot.Models;
+using EchoBot;
+
+namespace EchoBot
+{
+    public class EchoBotBot : IBot
+    {
+        private readonly EchoBotAccessors _accessors;
+        private readonly ILogger _logger;
+
+        public EchoBotBot(EchoBotAccessors accessors, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _logger = loggerFactory.CreateLogger<EchoBotBot>();
+            _logger.LogTrace("Turn start.");
+            _accessors = accessors ?? throw new System.ArgumentNullException(nameof(accessors));
+        }
+
+        public async Task OnTurnAsync(ITurnContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (context.Activity.Type == ActivityTypes.Message)
+            {
+                var response = await CustomQnAMaker.GetResults(context.Activity.Text);
+                var qnaResponse = JsonConvert.DeserializeObject<QnaResponse>(response);
+
+                if (response != "failure")
+                {
+                    //if (!response.Any())
+                    if(qnaResponse.answers[0].score < 10.0)
+                    {
+                        await context.SendActivityAsync("回答が見つかりませんでした。");
+                    }
+                    else
+                    {
+                        await context.SendActivityAsync($"{qnaResponse.answers[0].answer}");
+
+                        /*
+                        if (qnaResponse.answers.Count() >= 2)
+                        {
+                            await context.SendActivityAsync($"自信がないので他の回答もお伝えします\n\n{qnaResponse.answers[1].answer}");
+                        }
+                        */
+                    }
+                }
+            }
+            else
+            {
+                //await context.SendActivityAsync($"{context.Activity.Type} event detected");
+                await context.SendActivityAsync($"ようこそ！");
+            }
+           
+        }
+
+    }
+}
---
+++ v4QnABot/EchoBot/Models/CustomQnAMaker.cs
@@ -0,0 +1,50 @@
+﻿// Licensed under the MIT License.
+
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace EchoBot.Models
+{
+    public class CustomQnAMaker
+    {
+        /* 以下の3種類のキーを入力してください */
+
+        private const string KnowledgebaseId = "9bff38f0-f112-4923-89d9-fcb44bc67a84";
+        private const string Host = "https://myveriqna.azurewebsites.net/qnamaker";
+        private const string EndpointKey = "e538dc60-9f6d-4d23-a6f2-6c57b3f1bdaf";
+
+        private const string RequestUri = Host + "/knowledgebases/" + KnowledgebaseId + "/generateAnswer/";
+        public static async Task<string> GetResults(string message)
+        {
+            var question = $"{{\"question\": \"{message}\", \"top\": 5}}";
+
+            using (var client = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage())
+                {
+                    request.Method = HttpMethod.Post;
+                    request.RequestUri = new Uri(RequestUri);
+                    request.Content = new StringContent(question, Encoding.UTF8, "application/json");
+                    request.Headers.Add("Authorization", "EndpointKey " + EndpointKey);
+
+                    using (var response = await client.SendAsync(request))
+                    {
+                        if(response.IsSuccessStatusCode)
+                        {
+                            string json = await response.Content.ReadAsStringAsync();
+                            return json;
+                        }
+
+                        string failure = "failure";
+                        return failure;
+                    }
+
+                }
+            }
+
+        }
+    }
+}
